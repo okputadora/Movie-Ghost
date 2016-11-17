805b4444bcce01abf1c5293706cdac7fe8ffcade
okputadora <okputadora@gmail.com>
2016-11-17 12:01:51 -0500

    Redoing query functions Didn't have a good understanding of asynchronous functions and callbacks

--- mg.js
+++ mg.js
@@ -15,51 +15,26 @@ var trailId = "";
 var players = [];
 var gameOn = true;
 var cast = [];
-var titleIdAndCast = [];
+var title = "";
+var id = 0;
 // 0 ez; 1 md; 2 hd
 var difficulty = 0;
-
+function printResult(title, year, id){
+  console.log("Print result");
+  console.log(title);
+  console.log(id);
+  $("#p1 > h4:nth-child(3)").append(title);
+}
 function getRandomMovie(){
-    movieId = Math.floor(Math.random() * 1000);
-    console.log(movieId);
-    var id = movieId.toString();
-    url = baseId + id + key + lang;
-    $.getJSON(url, getMovieById, "jsonp");
-    return titleIdAndCast;
-}
-
-function searchMovie(data){
-  var id = data.results[0].id;
-  if (id = undefined){
-    // The input was invalid
-  }
-  // get the cast list
-  var url = baseId + id + "/credits" + key + lang;
-  $.getJSON(url, getMovieById, "jsonp");
-}
-
-function getMovieById(data){
-  var title = data.title;
-  var id = data.id;
-  console.log(id, title);
-  console.log(titleIdAndCast);
-  titleIdAndCast.push(title);
-  titleIdAndCast.push(id);
-  // get the cast
-  url = baseId + id + "/credits" + key + lang;
-  $.getJSON(url, getCast, "jsonp");
-  titleIdAndCast.push(cast);
-  }
-
-function getCast(data){
-  for (var x = 0; x < data.cast.length; x++){
-    cast.push(data.cast[x].name);
-    console.log(data.cast[x].name);
-  }
-}
-
-function searchActor(data){
-
+  id =  Math.floor(Math.random() * 1000);
+  var idStr = id.toString();
+  url = baseId + idStr + key + lang;
+  $.getJSON(url, function(data){
+    title = data.title;
+    id = data.id;
+    var year = data.release_date.slice(0,4);
+    printResult(title, year, id);
+  }, "jsonp");
 }
 
 function initiateGame(){
@@ -68,50 +43,39 @@ function initiateGame(){
     if (i >= players.length){
       i = i%players.length;
     }
-    if (players[i] === "robot" && trail.length === 0){
-      // Pick a random actor or movie
-      var actOrMove = Math.floor(Math.random() * 2)
-        // get random movie
-        titleIdAndCast = getRandomMovie();
-        var id = titleIdAndCast[0];
-        var title = titleIdAndCast[1];
+    if (players[i] === "robot"){
+      if (trail.length === 0){
+        // Pick a random actor or movie
+        var actOrMove = Math.floor(Math.random() * 2)
+          // get random movie
+          getRandomMovie();
 
-        // get the cast list
+          // get the cast list
 
-        if (actOrMove === 1){
-          // append to trail
-          trail.push(title);
-          trailId = "movie";
-          console.log(trailId);
+          if (actOrMove === 1){
+            // append to trail
+            trail.push(title);
+            trailId = "movie";
+            console.log(trailId);
+          }
+          else if (actOrMove === 0){
+            trail.push(cast[0]);
+            trailId = "actor";
+            console.log(trailId)
+          }
         }
-        else if (actOrMove === 0){
-          trail.push(cast[0]);
-          trailId = "actor";
-          console.log(trailId)
-        }
-        // display answer
-        // element.html(trail[-1])
-        gameOn = false;
-      }
+      else{}
+      // display answer
 
+        // element.html(trail[-1]
+    }
     else if (players[i] === "human" && trail.length === 0){
+      if (trail.length === 0){
 
-    }
-    else if (players[i] === "robot"){
-
-    }
-    else if (players[i] === "human"){
-      if (trailId === "movie"){
-        // when the user submits an actor check it
-        // against the cast list
       }
-      else if (trailId === "actor"){
-        // search the movie
-        // get the cast
-        // check if actor is in cast list
-      }
+      else{}
     }
-
+    gameOn = false;
   }
 }
 
@@ -149,14 +113,14 @@ $(document).ready(function(){
         // Append main viewer
         $("#active-players").append("<div class='avatar'id='p" + num +"'>" +
                                     "<h2>Player " + num + "</h2><div class='name'>" +
-                                    "<h3>Robot</h3></div>");
-        // document.getElementById("active-players").appendChild("div");
+                                    "<h3>Robot</h3></div><h4></h4>");
+        // document.getElementById("active-players").appendChild("div");/
       }
       if (players[i] === "human"){
         // Append main viewer
         console.log("Human appended");
         $("#active-players").append("<div class='avatar'id='p" + num +
-                            "'<h2>Player 1</h2><div class='prompt'><h3>" +
+                            "'><h2>Player " + num + "</h2><div class='prompt'><h3>" +
                             "Enter a movie or an actor</h3></div><div class='input'>" +
                             "<input id='searchTerm' type='text' placeholder=" +
                             "'Movie or Actor...' /><i id='submit' class='fa fa-arrow-right'" +
